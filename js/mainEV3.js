38b0fc0be8d62e1dc9c80a3910f95a2da6409a26
PugaUpsin1 <2020030282@upsin.edu.mx>
2022-11-09 15:11:28 -0700

    Correccion

---
+++ js/mainEV3.js
@@ -0,0 +1,54 @@
+function cotizar(){
+    if(document.getElementById("valor").value > 0 && document.getElementById("planes").value != ""){ 
+
+        let valorAuto = document.getElementById("valor").value;
+        let enganche = valorAuto *.30;
+        document.getElementById("txtEnganche").value = enganche;    
+        let mes = document.getElementById("planes").value;
+        let interes;
+        let num;
+        switch(mes){
+            case "12":
+                interes = 12.5;
+                num = 12;
+                break;
+
+            case "18":
+                interes = 17.2;
+                num = 18;
+                break;
+
+            case "24":
+                interes = 21;
+                num = 24;
+                break;
+
+            case "36":
+                interes = 26;
+                num = 36;
+                break;
+
+            case "48":
+                interes = 45;
+                num = 48
+                break;
+        } 
+        let valorConEnganche = valorAuto - enganche;
+        let totalFinanciar = ((valorAuto - enganche) + ((valorAuto - enganche) * interes/100));
+        document.getElementById("txtFinanciar").value = totalFinanciar
+        pagoMensual = totalFinanciar/num;
+        document.getElementById("txtPago").value = pagoMensual
+    }else{
+        alert("Opcion no valida")
+    }
+    
+    
+}
+
+function limpiar(){
+    document.getElementById('valor').value="";
+    document.getElementById('total').value ="";
+    document.getElementById('Pago').value =""; 
+    document.getElementById('planes').value ="";
+    document.getElementById('enganche').value="";
+}
